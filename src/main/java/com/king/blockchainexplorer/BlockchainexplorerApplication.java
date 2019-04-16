c722b149b4fe63c19ade980e0504a8b79674d003
筱翊 <keaide772349718@foxmail.com>
2019-04-16 11:31:27 +0800

    first git

---
+++ src/main/java/com/king/blockchainexplorer/BlockchainexplorerApplication.java
@@ -0,0 +1,13 @@
+package com.king.blockchainexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainexplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainexplorerApplication.class, args);
+	}
+
+}
