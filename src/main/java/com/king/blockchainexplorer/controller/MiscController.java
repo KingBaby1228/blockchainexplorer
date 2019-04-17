df1c0db0630a24c0a700927bbaf2e6b57fd46ab5
筱翊 <keaide772349718@foxmail.com>
2019-04-17 21:24:23 +0800

    add some misc api

--- src/main/java/com/king/blockchainexplorer/controller/MiscController.java
+++ src/main/java/com/king/blockchainexplorer/controller/MiscController.java
@@ -1,5 +1,6 @@
 package com.king.blockchainexplorer.controller;
 
+import com.king.blockchainexplorer.dto.ImportStateDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,24 @@ public class MiscController {
 
     @GetMapping("/search")
     public Object search(@RequestParam String keyword){
+        return null;
+    }
+
+    /**
+     * 同步系列
+     */
+    @GetMapping("/importFromHeight")
+    public void importFromHeight(@RequestParam Integer blockHeight, Boolean isClean){//是否需要清空
+
+    }
+
+    @GetMapping("/importFromHash")
+    public void importFromHash(@RequestParam String blockhash,Boolean isClean){
+
+    }
+
+    @GetMapping("/getImportState")
+    public ImportStateDTO getImportState(){
         return null;
     }
 }
