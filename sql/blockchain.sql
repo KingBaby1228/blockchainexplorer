7274312f08b4ae6d338b964f81fc58e251e7fef8
筱翊 <keaide772349718@foxmail.com>
2019-04-19 20:47:31 +0800

    add inital block chain data

--- sql/blockchain.sql
+++ sql/blockchain.sql
@@ -12,3 +12,8 @@ CREATE TABLE `blockchain` (
   PRIMARY KEY (`blockchain_id`),
   UNIQUE `idx_name_type`(`name`,`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 auto_increment=1;
+
+INSERT INTO `blockchain` VALUES ('1', 'Bitcoin', 'main', 'BTC');
+INSERT INTO `blockchain` VALUES ('2', 'Bitcoin', 'testnet', 'BTCTest');
+INSERT INTO `blockchain` VALUES ('3', 'Ethereum', 'main', 'ETH');
+
